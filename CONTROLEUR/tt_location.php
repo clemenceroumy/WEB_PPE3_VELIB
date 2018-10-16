7adc4a370cee96c3624b1824cc0b3bb1077f0f67
clemence roumy <clemenceroumy@hotmail.fr>
2018-10-16 17:22:09 +0200

    V2 ->  je loue: enregistre la date et heure de début de location et met le vélo à l'état L (louer)

--- CONTROLEUR/tt_location.php
+++ CONTROLEUR/tt_location.php
@@ -1,18 +1,23 @@
 <?php
 session_start();
 require_once('../MODELE/LocationModele.class.php');
+require_once('../MODELE/VehiculeModele.class.php');
 
 $idVelo= $_POST['numVelo'];
 $idAdherent= $_SESSION['idAdherent'];
 $dateLocation= $_POST['dateLocation'];
 
 $Location= new LocationModele();
+$Vehicule= new VehiculeModele();
 
 try{
     $ajoutLocation= $Location->ajouterLocation($idVelo,$idAdherent,$dateLocation);
+    $Vehicule->changeEtat('L',$idVelo); 
+    
     header("location: ../VUE/locationVelo.php");
 
-} catch{
+} 
+catch (Exception $e){
     echo "Erreur lors de l'insertion de la location";
 }
 
